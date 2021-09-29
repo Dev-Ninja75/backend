b43d4b1e23efc18138fc84ee5f1c99054a040841
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-09-29 15:13:06 +0200

    formulaire finnished

--- chapitre_02/formulaires/server.js
+++ chapitre_02/formulaires/server.js
@@ -8,12 +8,17 @@ app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 app.get("/", function (req, res) {
-  res.render("home.handlebars");
+  res.render("home");
 });
 
 app.post("/login", function (req, res) {
   console.log("username", req.body.username);
   console.log("password", req.body.password);
+  res.redirect("redirect");
+});
+
+app.get("/redirect", function (req, res) {
+  res.render("redirect");
 });
 
 app.listen(3000);
