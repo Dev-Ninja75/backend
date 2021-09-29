1e99158027c9661ada245ef0e9f957af1d0e7892
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-09-29 15:17:47 +0200

    finnished

--- chapitre_02/formulaires/server.js
+++ chapitre_02/formulaires/server.js
@@ -8,17 +8,18 @@ app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 app.get("/", function (req, res) {
+  // get end point route for home page
   res.render("home");
 });
 
 app.post("/login", function (req, res) {
   console.log("username", req.body.username);
   console.log("password", req.body.password);
-  res.redirect("redirect");
+  res.redirect("redirect"); //post redirect after login at redirect page
 });
 
 app.get("/redirect", function (req, res) {
-  res.render("redirect");
+  res.render("redirect"); // redirect page end point
 });
 
 app.listen(3000);
