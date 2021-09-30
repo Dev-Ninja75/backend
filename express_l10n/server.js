39d8b3778ae952b33e0ac113f307c0af49ceb15c
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-09-30 02:53:30 +0200

    add route fr and translat function

--- express_l10n/server.js
+++ express_l10n/server.js
@@ -1,8 +1,30 @@
+// init project Localization
 var express = require("express");
 var exphbs = require("express-handlebars");
 var app = express();
 
-app.engine("handlebars", expressHandlebars());
+app.use(express.static("public"));
+app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-app.listen(3000); // openin server
+var translations = require("./translations.json");
+
+// route "fr" // get end point route for homelng page
+app.get("/", function (req, res) {
+  res.redirect("/fr");
+});
+
+// route "lang" // get end point route for homelng translat
+app.get("/:lang?", function translat(req, res) {
+  var lang = req.params.lang;
+  var translat = translations[lang];
+  if (!translat) res.render("homelng", translations["fr"]);
+  res.render("homelng", translat);
+});
+
+// app.post("/fr", function (req, res) {
+//   res.redirect("homelng");
+// });
+
+// openin server
+app.listen(3000);
