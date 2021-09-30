0de9aea42ee60617162fca90bbe3352eb02c0a20
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-09-30 10:22:23 +0200

    final formulaires commit

--- chapitre_02/formulaires/server.js
+++ chapitre_02/formulaires/server.js
@@ -1,25 +1,29 @@
+// setup server
 var express = require("express");
 var exphbs = require("express-handlebars");
 var app = express();
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+// get end point route for home page
 app.get("/", function (req, res) {
-  // get end point route for home page
   res.render("home");
 });
 
 app.post("/login", function (req, res) {
   console.log("username", req.body.username);
   console.log("password", req.body.password);
-  res.redirect("redirect"); //post redirect after login at redirect page
+  res.redirect("redirect"); //post & redirect after login at redirect page
 });
 
 app.get("/redirect", function (req, res) {
   res.render("redirect"); // redirect page end point
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("server is running at port: " + 3000);
+});
